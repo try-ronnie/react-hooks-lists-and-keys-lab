9b6ec39b741aa830af3f6214b3a0abf4295c5045
ronniegikandi <kiiruronnie42@gmil.com>
2025-10-22 03:50:01 +0300

    completed lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -7,9 +7,12 @@ function ProjectItem({ name, about, technologies }) {
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((technology) => (
+          <span key={technology}>{technology}</span>
+        ))}
+
       </div>
     </div>
   );
 }
-
 export default ProjectItem;
